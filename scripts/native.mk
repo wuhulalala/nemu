81d4b618fc367e1f543fa947712f76577e471e5a
zsh <1796248596@qq.com>
2025-01-07 17:27:32 +0800

    add Count the total number of rows functionality

--- scripts/native.mk
+++ scripts/native.mk
@@ -41,6 +41,9 @@ gdb: run-env
 	$(call git_commit, "gdb NEMU")
 	gdb -s $(BINARY) --args $(NEMU_EXEC)
 
+count:
+	@find $(NEMU_HOME) -type f \( -name "*.c" -o -name "*.h" \)  -exec grep -c '.' {} + | awk -F : '{s += $$2} END {print s}'
+
 clean-tools = $(dir $(shell find ./tools -maxdepth 2 -mindepth 2 -name "Makefile"))
 $(clean-tools):
 	-@$(MAKE) -s -C $@ clean
